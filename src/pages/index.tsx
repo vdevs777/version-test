23a47099ae07dfb554e239d5e32eda889898133d
vdevs777 <joaovsousamnm@gmail.com>
2025-09-01 16:04:58 -0300

    BREAKING CHANGES: test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,7 +27,6 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               src/pages/index.tsx
             </code>
